f92bbceea01e6fe7d70b8e11547cfa1ee6e7cb4c
dumy10 <146924332+dumy10@users.noreply.github.com>
2024-12-07 02:58:38 +0200

    Updated the GalleryComponent

--- front-end/image-processing/src/app/app.config.ts
+++ front-end/image-processing/src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { IMAGE_CONFIG } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true,
+        disableImageLazyLoadWarning: true,
+      },
+    },
+  ],
 };
--- front-end/image-processing/src/app/gallery/gallery.component.ts
+++ front-end/image-processing/src/app/gallery/gallery.component.ts
@@ -1,33 +1,89 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { ImageService } from '../services/image.service';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageService } from '../services/image.service';
+import { ImageModel } from '../models/ImageModel';
+import { LoadingComponent } from '../loading/loading.component';
+import { ImageDialogComponent } from '../image-dialog/image-dialog.component';
 
 @Inject('ImageService')
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingComponent, MatPaginatorModule],
   providers: [ImageService],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss',
 })
 export class GalleryComponent implements OnInit {
-  // Will need an array with the images
-  imagePairs: Array<{ original: string; filtered: string }> = [];
-  constructor(private imageService: ImageService) {}
+  loading: boolean = false;
+  loadingMessage: string = 'Loading images...';
+  imagePairs: Array<{ originalImage: ImageModel; filteredImage: ImageModel }> =
+    [];
+  paginatedImagePairs: Array<{
+    originalImage: ImageModel;
+    filteredImage: ImageModel;
+  }> = [];
+
+  currentPage: number = 0;
+  itemsPerPage: number = 5;
+  totalPages: number = 1;
+
+  constructor(private dialog: MatDialog, private imageService: ImageService) {}
 
   ngOnInit(): void {
-    /* 
-      TO DO:
-      - Fetch images from the server
-      - Display already processed images in the gallery
-      */
-    this.imageService.getImages().subscribe(() => {
-      // TO DO: Implement the logic to display the images in the gallery
+    this.loading = true;
+    this.loadImages();
+  }
+
+  loadImages() {
+    this.imageService.getImages().subscribe({
+      next: (response: ImageModel[]) => {
+        response.forEach((image) => {
+          if (!image.parentUrl) {
+            return;
+          }
+
+          this.imagePairs.push({
+            originalImage: response.find(
+              (img) => img.id === image.parentId
+            ) as ImageModel,
+            filteredImage: image,
+          });
+        });
+        this.updatePagination();
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Failed to fetch images', error);
+        this.loading = false;
+        alert('Failed to fetch images');
+      },
+      complete: () => {
+        this.loading = false;
+      },
     });
   }
 
-  openDialog(image: string) {
-    // TO DO: Implement the dialog to display the original and filtered images
+  updatePagination() {
+    this.totalPages = Math.ceil(this.imagePairs.length / this.itemsPerPage);
+    const startIndex = this.currentPage * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedImagePairs = this.imagePairs.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: any) {
+    this.currentPage = event.pageIndex;
+    this.itemsPerPage = event.pageSize;
+    this.updatePagination();
+  }
+
+  openDialog(image: ImageModel) {
+    const dialogRef = this.dialog.open(ImageDialogComponent, { data: image });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 }
