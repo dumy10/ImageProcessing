0b6ac42ec300678fac178482c3d0dd791f6723b0
Dumitru Horovei <146924332+dumy10@users.noreply.github.com>
2025-01-28 20:42:10 +0200

    Dependency update

--- front-end/image-processing/src/app/app.config.ts
+++ front-end/image-processing/src/app/app.config.ts
@@ -3,19 +3,11 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { IMAGE_CONFIG } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
-    {
-      provide: IMAGE_CONFIG,
-      useValue: {
-        disableImageSizeWarning: true,
-        disableImageLazyLoadWarning: true,
-      },
-    },
   ],
 };
