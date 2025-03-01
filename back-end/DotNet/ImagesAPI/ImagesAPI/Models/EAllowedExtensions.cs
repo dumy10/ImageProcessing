3e80b98ee77cdaed594bf61327ea94ad077cf97a
Dumitru Horovei <146924332+dumy10@users.noreply.github.com>
2025-03-01 21:03:09 +0200

    Added an enum with the allowed extensions. Created tests for the collection service.

---
+++ back-end/DotNet/ImagesAPI/ImagesAPI/Models/EAllowedExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace ImagesAPI.Models
+{
+    /// <summary>
+    /// Enum for allowed extensions
+    /// </summary>
+    public enum EAllowedExtensions
+    {
+        /// <summary>
+        /// PNG extension
+        /// </summary>
+        PNG,
+        /// <summary>
+        /// JPEG extension
+        /// </summary>
+        JPEG,
+        /// <summary>
+        /// JPG extension
+        /// </summary>
+        JPG
+    }
+}
