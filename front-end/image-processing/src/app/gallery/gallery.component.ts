0dcd9da1d6e18d8cd8368fa12e8b83a8b5fae502
Dumitru Horovei <146924332+dumy10@users.noreply.github.com>
2025-06-22 00:47:24 +0300

    Removed unused import

--- front-end/image-processing/src/app/gallery/gallery.component.ts
+++ front-end/image-processing/src/app/gallery/gallery.component.ts
@@ -19,7 +19,6 @@ import {
   ErrorAction,
   ErrorBannerComponent,
 } from '../shared/error-banner/error-banner.component';
-import { environment } from '../../environments/environment';
 
 /**
  * GalleryComponent is a component that displays a gallery of images with pagination.
